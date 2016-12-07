4107841e0b09217e280715025e6a75efcd5a0096
Tasha Casagni <casagni708@hotmail.com>
2016-12-07 08:37:35 -0600

    inital setup

---
+++ server.js
@@ -0,0 +1,11 @@
+/***    SERVER.JS PSEUDOCODE    ***/
+
+    //Require our Dependencies and routes.js
+
+    //Serve up our assets folder
+
+    //Use the bodyparser
+
+    //Pull in our routes
+
+    //Listen to Port
